aa9dfed20ff4ec2af426838085812474b1d62838
R-Pavel <prylsky@gmail.com>
2022-03-23 22:54:57 +0700

    work_with_mongo

--- app.js
+++ app.js
@@ -4,28 +4,63 @@ const express = require('express');
 const app = express();
 const api_url = process.env.API_URL
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const userDB = 'eshop-user';
+const pass = 'bYji73E8';
+
 
 
 //middleware
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
-app.get(`${api_url}/products`,(req, res) => {
-	const product = {
-		id:1,
-		name: "good1",
-		desc: 'good1 very good',
-		image: 'some_url_pic', 
-		price: "100",
 
-	};
-	res.send(product);
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+	countInStock: {
+		type: Number,
+		required: true
+	}
+});
+
+const Product = mongoose.model('Product', productSchema)
+
+app.get(`${api_url}/products`, async (req, res) => {
+	const productList = await Product.find();
+	if(!productList){
+		res.status(500).json({success:false})
+	}
+	res.send(productList);
 })
 
 app.post(`${api_url}/products`,(req, res) => {
+	const product = new Product({
+		name: req.body.name,
+		image: req.body.image,
+		countInStock: req.body.countInStock
+	})
 
-	const newProduct = req.body;
-	console.log(newProduct);
-	res.send(newProduct);
+	product.save().then((createdProduct => {
+		res.status(201).json(createdProduct)
+	})).catch((err) => {
+		res.status(500).json({
+			error:err, 
+			success: false
+		})
+	})
+})
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+.then(() => {
+	console.log("Connection with database is ready")
+})
+.catch((error) => {
+	console.log(error)
 })
 
 app.listen(3000, ()=> {
